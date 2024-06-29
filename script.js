572b29ddf85e4817ec9aa3cdb8f47d81248bc557
Rafael021-pro <rafaelgds021@gmail.com>
2024-06-29 20:13:23 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 var playlistId = 'PLJKGsLV6NngX7FyUPNOrCv3YNawUmQGxu'; // ID da sua playlist
-var apiUrl = 'https://www.googleapis.com/youtube/v3/playlistItems';
 var apiKey = 'YOUR_YOUTUBE_API_KEY'; // Substitua pela sua YouTube API Key
 
-// Função para carregar e reproduzir vídeo da playlist
-function loadPlaylistVideo() {
+// Função para obter vídeo aleatório da playlist
+function fetchRandomVideo(callback) {
+    var apiUrl = 'https://www.googleapis.com/youtube/v3/playlistItems';
     var requestUrl = apiUrl + '?part=snippet&maxResults=50&playlistId=' + playlistId + '&key=' + apiKey;
 
     fetch(requestUrl)
@@ -12,20 +12,26 @@ function loadPlaylistVideo() {
             var items = data.items;
             var randomIndex = Math.floor(Math.random() * items.length);
             var videoId = items[randomIndex].snippet.resourceId.videoId;
-            var embedUrl = 'https://www.youtube.com/embed/' + videoId + '?autoplay=1';
-
-            document.getElementById('player').src = embedUrl;
+            callback(videoId);
         })
         .catch(error => console.error('Erro ao carregar a playlist:', error));
 }
 
-// Função para verificar e atualizar a playlist automaticamente a cada 5 minutos
-function autoUpdatePlaylist() {
-    setInterval(loadPlaylistVideo, 300000); // 300000 ms = 5 minutos
+// Função para carregar e reproduzir áudio da playlist
+function loadPlaylistAudio() {
+    fetchRandomVideo(function(videoId) {
+        var audioUrl = 'https://www.youtube.com/watch?v=' + videoId;
+        document.getElementById('player').src = audioUrl;
+    });
 }
 
-// Chamar a função para carregar e iniciar a reprodução do vídeo da playlist
-loadPlaylistVideo();
+// Função para verificar e atualizar a playlist automaticamente a cada 5 minutos
+function autoUpdatePlaylist() {
+    setInterval(loadPlaylistAudio, 300000); // 300000 ms = 5 minutos
+}
+
+// Chamar a função para carregar e iniciar a reprodução do áudio da playlist
+loadPlaylistAudio();
 
 // Chamar a função para iniciar a verificação automática da playlist
 autoUpdatePlaylist();
